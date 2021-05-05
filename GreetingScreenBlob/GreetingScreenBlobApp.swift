8532723777ec2d81c4cf1f37afeb30bcc0a1c0e7
deniswift <deniswiftui@gmail.com>
2021-05-05 10:12:27 +0300

    Initial Commit

---
+++ GreetingScreenBlob/GreetingScreenBlobApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingScreenBlobApp.swift
+//  GreetingScreenBlob
+//
+//  Created by Denis Larin on 05.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct GreetingScreenBlobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
